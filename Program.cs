19f5c648b2b8325385c7969c8b6eebd9d9a068a6
Miguel Mendes <mendesmiguel2007@gmail.com>
2026-04-23 22:25:23 -0300

    Tempo

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿﻿double TamanhoBytes, VelocidadeMbps, TempoMin;
+
+
+Console.WriteLine("--- Tempo de Download ---\n");
+
+Console.Write("Tamanho do arquivo em MB....: ");
+TamanhoBytes = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Velocidade da conexão em Mbps...: ");
+
+VelocidadeMbps = Convert.ToDouble(Console.ReadLine());
+
+TempoMin = TamanhoBytes * 8 / VelocidadeMbps / 60;
+
+Console.WriteLine($"\nTempo estimado de download: {TempoMin:N1} minutos");
